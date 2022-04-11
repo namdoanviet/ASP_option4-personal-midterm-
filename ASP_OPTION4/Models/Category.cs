fa660c3b35e3d1220f7e2f72472d7558defbebbb
NAM ĐOÀN VIỆT <19120297@student.hcmus.edu.vn>
2022-04-11 23:40:33 +0700

    finish create component

--- ASP_OPTION4/Models/Category.cs
+++ ASP_OPTION4/Models/Category.cs
@@ -8,7 +8,8 @@ namespace ASP_OPTION4.Models
 
         [Required]
         public string Name { get; set; }
-
+        [Display(Name="Display Order")]
+        [Range(1,100,ErrorMessage="Display Order must be between 1 and 100 onlly!!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
