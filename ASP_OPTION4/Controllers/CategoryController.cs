d7f6c2285057a06b1b71dde7dee57e51ae621ffd
NAM ĐOÀN VIỆT <19120297@student.hcmus.edu.vn>
2022-04-12 13:32:55 +0700

    finish project

--- ASP_OPTION4/Controllers/CategoryController.cs
+++ ASP_OPTION4/Controllers/CategoryController.cs
@@ -34,9 +34,74 @@ namespace ASP_OPTION4.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Create successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id ==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The displayorder cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Edit successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Delete successfully";
+
+            return RedirectToAction("Index");
         }
     }
 }
